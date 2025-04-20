f649e5b13637385610d5292fac6c27854769ce82
omi-kuzi <taichi6nana8@gmail.com>
2025-04-20 11:54:13 +0900

    Update calendar.js

--- js/calendar.js
+++ js/calendar.js
@@ -1,1 +1,16 @@
+// カレンダーを動的に表示
+const events = {
+  '2025-04-10': '定例会',
+  '2025-04-15': '新入生歓迎会',
+  // 他のイベント...
+};
+
+// カレンダーの表示
+document.querySelectorAll('td').forEach(td => {
+  const day = td.innerText;
+  if (events[`2025-04-${day}`]) {
+    td.style.backgroundColor = '#FFDFDF';
+    td.title = events[`2025-04-${day}`];
+  }
+});
 
